742fa55cf90aa570d0566758d2929d6c4581cc67
Caroline Galecki <carolinegalecki@gmail.com>
2025-12-06 01:01:35 -0500

    fuck the padding isnt uniform in the original problem

--- day_6/solution.py
+++ day_6/solution.py
@@ -42,3 +42,62 @@ def part1(input_text):
   
   return grand_total_sum
 
+
+# jesus christ gnome math
+def part2(input_text):
+  homework_map = parse_input_2d(input_text)
+  grand_total_sum = 0
+  # transpose so we can loop through columns then rows
+  transposed_homework_map = zip(*homework_map)
+  print_map(homework_map)
+
+  for col_index, col_val in enumerate(transposed_homework_map):
+    # grab the last item so we know the operator
+    operator = col_val[len(col_val) - 1]
+    math_answer = 0
+    highest_val_char_count = 0
+    # numbers are max 4 digits looks like
+    answer_array = [0, 0, 0, 0]
+
+    # do a quick loop to determine max character count in row? aka 4 character num like 1000 determines what we add to
+    for row_index, row_val in enumerate(col_val):
+      char_count = len(row_val) - 1
+      if char_count > highest_val_char_count:
+        highest_val_char_count = char_count
+
+    # now loop over this with this knowledge in hand
+    for row_index, row_val in enumerate(col_val):
+      value_char_count = len(row_val) - 1
+      char_difference = highest_val_char_count - value_char_count
+      # pad our number with trash to ignore so that the counts match
+      padded_number = row_val + '@' * char_difference
+      print("current value we are looping over", row_val)
+
+      # now that our numbers match we can add / multiply them accordingly
+      for char_index, character in enumerate(padded_number):
+        if character == '@' or character == '*' or character == '+' or character == ' ':
+          # skip this character
+          continue
+    
+        print("answer array", answer_array)
+
+        if row_index == 0:
+          print(f"first index, answer array index {char_index} becomes {character}")
+          answer_array[char_index] = int(character)
+          continue
+
+        math_answer = answer_array[char_index]
+        if operator == '*':
+          print(f"multiply {math_answer} with {character} = {math_answer * int(character)}")
+          math_answer = math_answer * int(character)
+        else:
+          print(f"add  {math_answer} with {character} = {math_answer + int(character)}")
+          math_answer = math_answer + int(character)
+        answer_array[char_index] = math_answer
+
+    total_sum = sum(answer_array)
+    print(f"total sum = {total_sum}")
+    grand_total_sum = grand_total_sum + total_sum
+    print(f"grand total sum = {grand_total_sum}")
+  
+  return grand_total_sum
--- day_6/test_solution.py
+++ day_6/test_solution.py
@@ -1,6 +1,6 @@
 import pytest
 from pathlib import Path
-from solution import part1
+from solution import part1, part2
 
 
 def test_part1_example():
@@ -15,13 +15,13 @@ def test_part1_input():
     input_text = Path("input.txt")
 
     result = part1(input_text)
-    assert result == 1395
+    assert result == 5361735137219
 
 
-# def test_example_part2():
-#     input_text = Path("example.txt")
-#     result = part2(input_text)
-#     assert result == 43
+def test_example_part2():
+    input_text = Path("example.txt")
+    result = part2(input_text)
+    assert result == 3263827
 
 # def test_part2_input():
 #     input_text = Path("input.txt")
