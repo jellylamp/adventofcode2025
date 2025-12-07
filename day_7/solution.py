82fbd820e1018c78ad58693a82b3ae2220c97cb1
Caroline Galecki <carolinegalecki@gmail.com>
2025-12-07 15:00:26 -0500

    day 7 part 2

--- day_7/solution.py
+++ day_7/solution.py
@@ -94,5 +94,61 @@ def bfs(manifold_map, start_row, start_col, map_copy):
               })
   return split_count
 
+def part2(input_text):
+  manifold_map = parse_input_2d(input_text)
+  manifold_copy = copy.deepcopy(manifold_map)
+  start_col_index = manifold_map[0].index('S')
+  split_count = dfs_part2(manifold_map, 0, start_col_index)
+  return split_count
+
 def create_node_id(row, col):
   return f"r{str(row)} + c{str(col)}"
+
+
+# Switch to a DFS so we can take advantage of memoization as my other code just hung
+def dfs_part2(manifold_map, start_row, start_col):
+    map_row_bound = len(manifold_map) - 1
+    map_col_bound = len(manifold_map[0]) - 1
+    memo = {}
+    
+    def dfs_recursive(row, col):
+        # if we've taken the path, just jump to the answer instead of revisiting
+        node_id = create_node_id(row, col)
+        if node_id in memo:
+          return memo[node_id]
+
+        # reached the bottom row
+        if row == map_row_bound:
+            return 1
+        
+        path_count = 0
+        
+        # Look at the cell directly below
+        if row + 1 <= map_row_bound:
+            node_to_consider_row = row + 1
+            node_to_consider_col = col
+            node_to_consider = manifold_map[node_to_consider_row][node_to_consider_col]
+            
+            if node_to_consider == '.':
+                # Continue straight down
+                path_count += dfs_recursive(node_to_consider_row, node_to_consider_col)
+            
+            if node_to_consider == '^':
+                # oh lawd we splittin, do the two items to the SIDE of the splitter
+                # left side
+                node_to_consider_row = row + 1
+                node_to_consider_col = col - 1
+                if node_to_consider_col < map_col_bound and node_to_consider_col >= 0:
+                    path_count += dfs_recursive(node_to_consider_row, node_to_consider_col)
+                
+                # right side
+                node_to_consider_row = row + 1
+                node_to_consider_col = col + 1
+                if node_to_consider_col >= 0 and node_to_consider_col <= map_col_bound:
+                    path_count += dfs_recursive(node_to_consider_row, node_to_consider_col)
+
+        # add the path to the memo
+        memo[node_id] = path_count
+        return path_count
+    
+    return dfs_recursive(start_row, start_col)
--- day_7/test_solution.py
+++ day_7/test_solution.py
@@ -1,34 +1,29 @@
 import pytest
 from pathlib import Path
-from solution import part1
+from solution import part1, part2
 
 
-def test_part1_example():
-    """Run part 1."""
-    input_text = Path("example.txt")
-
-    result = part1(input_text)
-    assert result == 21
-
-def test_part1_input():
-    """Run part 1."""
-    input_text = Path("input.txt")
-
-    result = part1(input_text)
-    assert result == 1553
-
-# def test_example_small_part2():
-#     input_text = Path("example-small.txt")
-#     result = part2(input_text)
-#     assert result == 5
-
-# def test_example_part2():
+# def test_part1_example():
+#     """Run part 1."""
 #     input_text = Path("example.txt")
-#     result = part2(input_text)
-#     assert result == 17
 
-# def test_part2_input():
+#     result = part1(input_text)
+#     assert result == 21
+
+# def test_part1_input():
+#     """Run part 1."""
 #     input_text = Path("input.txt")
 
-#     result = part2(input_text)
-#     assert result == 348115621205535
+#     result = part1(input_text)
+#     assert result == 1553
+
+def test_example_part2():
+    input_text = Path("example.txt")
+    result = part2(input_text)
+    assert result == 40
+
+def test_part2_input():
+    input_text = Path("input.txt")
+
+    result = part2(input_text)
+    assert result == 348115621205535
